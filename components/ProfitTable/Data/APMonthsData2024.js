a5b651ab416c7ec096ea265f4e0c86a87ac99fb8
LuTheDog <lukamatalzic@gmail.com>
2024-01-06 15:39:54 +0100

    New data

--- components/ProfitTable/Data/APMonthsData2024.js
+++ components/ProfitTable/Data/APMonthsData2024.js
@@ -2,7 +2,7 @@ export default [
   {
     id: 0,
     mesec: "Januar",
-    profit: "+15.6%",
+    profit: "+7.95%",
     dani: [
       {
         dan: "2.",
@@ -10,6 +10,20 @@ export default [
         ulog: "20%",
         profit: "+15.6%",
         prosao: true,
+      },
+      {
+        dan: "5.",
+        kvota: "1.49",
+        ulog: "15%",
+        profit: "+7.35",
+        prosao: true,
+      },
+      {
+        dan: "5.",
+        kvota: "1.37",
+        ulog: "15%",
+        profit: "-15%",
+        prosao: false,
       },
     ],
   },
--- components/ProfitTable/Data/JUMonthsData2024.js
+++ components/ProfitTable/Data/JUMonthsData2024.js
@@ -2,7 +2,7 @@ export default [
   {
     id: 0,
     mesec: "Januar",
-    profit: "+11.16%",
+    profit: "+10.71%",
     dani: [
       {
         dan: "2.",
@@ -16,6 +16,41 @@ export default [
         kvota: "1.80",
         ulog: "6%",
         profit: "-6%",
+        prosao: false,
+      },
+      {
+        dan: "3.",
+        kvota: "2.85",
+        ulog: "4%",
+        profit: "-4%",
+        prosao: false,
+      },
+      {
+        dan: "3.",
+        kvota: "1.40",
+        ulog: "20%",
+        profit: "+8%",
+        prosao: true,
+      },
+      {
+        dan: "5.",
+        kvota: "1.57",
+        ulog: "10%",
+        profit: "+5.7%",
+        prosao: true,
+      },
+      {
+        dan: "5.",
+        kvota: "1.97",
+        ulog: "5%",
+        profit: "+4.85%",
+        prosao: true,
+      },
+      {
+        dan: "5.",
+        kvota: "1.37",
+        ulog: "15%",
+        profit: "-15%",
         prosao: false,
       },
     ],
